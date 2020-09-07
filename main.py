406199bf053f44a4b2ec676d1baf0c041575f8d0
foxandrewd <fox.andrew.d@gmail.com>
2020-09-07 17:37:56 +0000

    Spacings

--- main.py
+++ main.py
@@ -64,83 +64,57 @@ class Board(Canvas):
 
     def checkAppleCollision(self):
         '''checks if the head of snake collides with apple'''
-
         apple = self.find_withtag("apple")
         head = self.find_withtag("head")
-
         x1, y1, x2, y2 = self.bbox(head)
         overlap = self.find_overlapping(x1, y1, x2, y2)
-
         for ovr in overlap:
-
             if apple[0] == ovr:
-
                 self.score += 1
                 x, y = self.coords(apple)
                 self.create_image(x, y, image=self.dot, anchor=NW, tag="dot")
                 self.locateApple()
 
-
     def moveSnake(self):
         '''moves the Snake object'''
-
         dots = self.find_withtag("dot")
         head = self.find_withtag("head")
-
         items = dots + head
-
         z = 0
         while z < len(items)-1:
-
             c1 = self.coords(items[z])
             c2 = self.coords(items[z+1])
             self.move(items[z], c2[0]-c1[0], c2[1]-c1[1])
             z += 1
-
         self.move(head, self.moveX, self.moveY)
-
 
     def checkCollisions(self):
         '''checks for collisions'''
-
         dots = self.find_withtag("dot")
         head = self.find_withtag("head")
-
         x1, y1, x2, y2 = self.bbox(head)
         overlap = self.find_overlapping(x1, y1, x2, y2)
-
         for dot in dots:
             for over in overlap:
                 if over == dot:
                   self.inGame = False
-
-        if x1 < 0:
+        if x1 < 0: self.inGame = False
+        if y1 < 0: self.inGame = False
+        if x1 > (Cons.BOARD_WIDTH - Cons.DOT_SIZE):
             self.inGame = False
-
-        if x1 > Cons.BOARD_WIDTH - Cons.DOT_SIZE:
+        if y1 > (Cons.BOARD_HEIGHT - Cons.DOT_SIZE):
             self.inGame = False
-
-        if y1 < 0:
-            self.inGame = False
-
-        if y1 > Cons.BOARD_HEIGHT - Cons.DOT_SIZE:
-            self.inGame = False
-
 
     def locateApple(self):
         '''places the apple object on Canvas'''
-
         apple = self.find_withtag("apple")
         self.delete(apple[0])
-
         r = random.randint(0, Cons.MAX_RAND_POS)
         self.appleX = r * Cons.DOT_SIZE
         r = random.randint(0, Cons.MAX_RAND_POS)
         self.appleY = r * Cons.DOT_SIZE
-
         self.create_image(self.appleX, self.appleY, anchor=NW,
             image=self.apple, tag="apple")
-
 
     def onKeyPressed(self, e):
         '''controls direction variables with cursor keys'''
@@ -162,13 +136,10 @@ class Board(Canvas):
             self.moveX = 0
             self.moveY = Cons.DOT_SIZE
 
-
     def onTimer(self):
         '''creates a game cycle each timer event'''
-
         self.drawScore()
         self.checkCollisions()
-
         if self.inGame:
             self.checkAppleCollision()
             self.moveSnake()
@@ -176,38 +147,28 @@ class Board(Canvas):
         else:
             self.gameOver()
 
-
     def drawScore(self):
         '''draws score'''
-
         score = self.find_withtag("score")
         self.itemconfigure(score, text="Score: {0}".format(self.score))
 
-
     def gameOver(self):
         '''deletes all objects and draws game over message'''
-
         self.delete(ALL)
         self.create_text(self.winfo_width() /2, self.winfo_height()/2,
             text="Game Over with score {0}".format(self.score), fill="white")
 
-
 class Snake(Frame):
-
     def __init__(self):
         super().__init__()
-
         self.master.title('Snake')
         self.board = Board()
         self.pack()
 
-
 def main():
-
     root = Tk()
     nib = Snake()
     root.mainloop()
-
 
 if __name__ == '__main__':
     main()
